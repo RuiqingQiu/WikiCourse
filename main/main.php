c676eaff189ddeccd45266be4489929246a02a82
Kang Tao <kevintao93@gmail.com>
2013-05-04 15:05:30 -0700

    main.php

---
+++ main/main.php
@@ -0,0 +1,67 @@
+
+<!--DCTYPE html-->
+<html>
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <title>WikiCourse登陆</title>
+    <link href="../css/main.css" rel="stylesheet" type="text/css" />
+   
+  </head>
+  
+  <body>
+    <h2>Wiki COURSE</h2>
+    <!--navigator-->
+    <nav> <a href="../main/index.html">找课</a> <a href="../main/index.html">求课</a>
+      <a href="../main/index.html">同课</a> <a href="../main/index.html">账户</a> <a
+
+        href="../main/index.html">关于</a> 
+    </nav>
+    
+    
+    <table>
+      <tr>     
+        <td>
+        <?php
+        mysql_connect("localhost","root","") or die (mysql_error());
+        mysql_select_db("db_wc") or die(mysql_error());
+	      $data = mysql_query("SELECT * FROM `wc_course`");
+	      if($data == FALSE)
+	      {
+	        die(mysql_error());
+	      }
+	      while($row = mysql_fetch_array($data))
+	      {
+         $value = $row["department"];
+        ?>
+	      <a href="#"><?php echo $value; ?></a><br/>
+	      <?php
+	      };
+        ?>
+     
+
+
+        </td>
+  
+        <td>
+          <table>
+            <tbody>
+              <tr>
+                <td>找不到？ <a href="../business/request_course.html">求课</a> </td>
+              </tr>
+              <tr>
+                <td>要补充？ <a href="../business/supply_course.html">供课</a> </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+     
+        <td>
+        <div>
+        悬赏排行榜
+        </div>
+        </td>
+      
+      </tr>
+    </table>
+  </body>
+</html>
